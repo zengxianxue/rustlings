ac91fc7cc8f604cb5f2b3b84a8e5d997975b4c27
zengxianxue <zengxianxue@tom.com>
2022-03-02 00:17:54 +0800

    完成内建类型

--- exercises/move_semantics/move_semantics5.rs
+++ exercises/move_semantics/move_semantics5.rs
@@ -3,13 +3,11 @@
 // adding, changing or removing any of them.
 // Execute `rustlings hint move_semantics5` for hints :)
 
-// I AM NOT DONE
-
 fn main() {
     let mut x = 100;
     let y = &mut x;
+    *y += 100; // 编译器推导 `mut y` 在此结束
     let z = &mut x;
-    *y += 100;
     *z += 1000;
     assert_eq!(x, 1200);
 }
